28cbcd09286a2075e484bb5c458f4f3c8ad767bd
Saurav Singh <sauravcloud26@gmail.com>
2026-02-16 23:17:37 +0530

    sabzi-mandi complete

--- src/04-sabzi-mandi.js
+++ src/04-sabzi-mandi.js
@@ -49,20 +49,37 @@
  */
 export function addToCart(cart, item) {
   // Your code here
+  if (!Array.isArray(cart)) return -1;
+  if (typeof item !== "string" || item.length === 0) {
+    return cart.length;
+  }
+  return cart.push(item);
 }
 
 export function addUrgentItem(cart, item) {
   // Your code here
+  if (!Array.isArray(cart)) return [];
+  if (typeof item !== "string" || item.length == 0) return cart;
+  cart.unshift(item);
+  return cart;
 }
 
 export function removeLastItem(cart) {
   // Your code here
+  if (!Array.isArray(cart) || cart.length === 0) return undefined;
+  const removedItem = cart.pop();
+  return removedItem;
 }
 
 export function isInCart(cart, item) {
   // Your code here
+  if (!Array.isArray(cart)) return false;
+  return cart.includes(item);
 }
 
 export function mergeCarts(cart1, cart2) {
   // Your code here
+  const newCart1 = !Array.isArray(cart1) ? [] : cart1;
+  const newCart2 = !Array.isArray(cart2) ? [] : cart2;
+  return newCart1.concat(newCart2);
 }
